0d449f5adb0f661be5ca34aaa79cf2c414b52919
thanhsondoan02 <thanhsondoan02@gmail.com>
2022-11-20 11:20:09 +0700

    init history

--- app/src/main/java/com/example/dcom/presentation/main/history/HistoryFragment.kt
+++ app/src/main/java/com/example/dcom/presentation/main/history/HistoryFragment.kt
@@ -5,4 +5,22 @@ import com.example.dcom.presentation.common.BaseFragment
 
 class HistoryFragment : BaseFragment(R.layout.history_fragment) {
 
+    override fun onInitView() {
+        super.onInitView()
+
+        setUpVariables()
+        setUpOnClick()
+        setUpRecyclerView()
+
+    }
+
+    private fun setUpVariables() {
+    }
+
+    private fun setUpOnClick() {
+    }
+
+    private fun setUpRecyclerView() {
+    }
+
 }
